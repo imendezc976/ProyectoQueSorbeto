ff613b3293194f845bcbccc3136a9117d18a9bf5
Santiago Méndez <imendezc976@ulacit.ed.cr>
2018-08-08 20:47:17 -0600

    Cambios funcionales, ya abre pantalla
    Abre pantalla productos.

--- app/src/main/java/santiagoyandres/proyectoquesorbeto/PantallaProducto.java
+++ app/src/main/java/santiagoyandres/proyectoquesorbeto/PantallaProducto.java
@@ -31,7 +31,7 @@ public class PantallaProducto extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pantalla_usuarios);
+        setContentView(R.layout.activity_pantalla_producto);
 
         baseDeDatos = new SQLite_Class(getApplicationContext());
 
